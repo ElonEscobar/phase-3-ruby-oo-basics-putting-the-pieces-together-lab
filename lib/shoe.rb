a246b6e025ee409e570564214663c9850155398b
Dan Muendo <danwalker254254@gmail.com>
2022-10-21 06:24:30 -0400

    assignment complete

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def author=(color)
+        @color = color
+    end
+
+    def page_count=(size)
+        @size = size
+    end
+
+    def genre=(material)
+        @material = material
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
